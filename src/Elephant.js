9a2583798369199ac140c3d383ac348c52c9aa83
Egg Sandwich <eggsandwich@Eggs-MacBook-Pro.local>
2019-02-18 11:31:37 -0700

    initial commit react zoo app

---
+++ src/Elephant.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Elephant extends Component {
+  render() {
+    return (
+      <h1>Hi, I'm an Elephant!</h1>
+    );
+  }
+}
+
+export default Elephant;
